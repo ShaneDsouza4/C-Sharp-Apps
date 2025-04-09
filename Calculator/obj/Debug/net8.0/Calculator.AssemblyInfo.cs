3607b7ed415dfd39d38ca59b79cfb638bf2c48db
ShaneDsouza4 <shane.json96@gmail.com>
2025-04-08 23:01:16 -0400

    Dice Game App

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5900b85177a29963bea2c49dc5eca8009b2e4a10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40933d094f3f3c0b93028016bd356d07ad0cffc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
