303bf9d98d64277b38dd3e4db1739ad72ead29d8
ShaneDsouza4 <shane.json96@gmail.com>
2025-04-10 19:24:06 -0400

    Shape Area Calculator

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3607b7ed415dfd39d38ca59b79cfb638bf2c48db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b556356dd5f6592ea429abe60d24888ac7b8ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ ShapeAreaCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("What shape would you like to find the area of ?");
+Console.WriteLine("Please enter 'r' for rectangle.");
+Console.WriteLine("Please enter any key for circle.");
+
+var userChoice = Console.ReadLine();
+float result;
+
+if(userChoice == "r")
+{
+    Console.WriteLine("Enter height of recatangle: ");
+    float height = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Enter width of rectangle");
+    float width = float.Parse(Console.ReadLine());
+
+    result = height * width;
+
+}
+else
+{
+    Console.WriteLine("Enter radius of circle: ");
+    float radius = float.Parse(Console.ReadLine());
+
+    result = (float)Math.PI * radius;
+}
+
+Console.WriteLine($"The result is {result}");
+
+
+Console.ReadKey();
