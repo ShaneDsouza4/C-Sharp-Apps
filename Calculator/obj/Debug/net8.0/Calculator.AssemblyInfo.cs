00003a0270619140be066e6003710704f75b1394
ShaneDsouza4 <shane.json96@gmail.com>
2025-04-11 23:44:46 -0400

    Turn Based Combat App

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b556356dd5f6592ea429abe60d24888ac7b8ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+303bf9d98d64277b38dd3e4db1739ad72ead29d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ TurnBasedCombat/Program.cs
@@ -0,0 +1,63 @@
+﻿int playerHp = 40;
+int enemyHp = 20;
+
+int playerAttack = 5;
+int enemyAttack = 7;
+
+int healAmount = 5;
+
+Random random = new Random();
+
+while (playerHp > 0 && enemyHp > 0)
+{
+    //Player turn
+    Console.WriteLine(" ");
+    Console.WriteLine("****Player Turn****");
+    Console.WriteLine($"Player HP: {playerHp}. Enemy HP: {enemyHp}");
+    Console.WriteLine("Press 'a' to attack or 'h' to heal.");
+
+    string choice = Console.ReadLine();
+
+    if (choice == "a")
+    {
+        enemyHp -= playerAttack;
+        Console.WriteLine($"Player attacked enemey. Damage done: {playerAttack}");
+    }
+    else if (choice == "h")
+    {
+        playerHp += healAmount;
+        Console.WriteLine($"Player has restored. Health Points: {playerHp}");
+    }
+
+    //Enemy Turn
+    if (enemyHp > 0)
+    {
+        Console.WriteLine(" ");
+        Console.WriteLine("****Enemy Turn****");
+        Console.WriteLine($"Player HP: {playerHp}. Enemy HP: {enemyHp}");
+        int enemyChoice = random.Next(0, 1);
+
+        if (enemyChoice == 0)
+        {
+            playerHp -= enemyAttack;
+            Console.WriteLine($"Enemy attacked player. Damage done: {enemyAttack}");
+        }
+        else if (enemyChoice == 1)
+        {
+            enemyHp += healAmount;
+            Console.WriteLine($"Enemy has restored. Health Points: {enemyHp}");
+        }
+    }
+}
+
+Console.WriteLine(" ");
+if (playerHp > 0)
+{
+    Console.WriteLine("Congratulations! You have won.");
+}
+else
+{
+    Console.WriteLine("You lost :(");
+}
+
+Console.ReadKey();
