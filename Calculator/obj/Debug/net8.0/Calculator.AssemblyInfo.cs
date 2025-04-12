3f31f316cf6e0f34fa849fd684d5250830a49531
ShaneDsouza4 <shane.json96@gmail.com>
2025-04-12 10:21:35 -0400

    Rock Paper Scissors Game

--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+303bf9d98d64277b38dd3e4db1739ad72ead29d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00003a0270619140be066e6003710704f75b1394")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,84 @@
+﻿int playerScore = 0;
+int enemyScore = 0;
+Random random = new Random();
+
+Console.WriteLine("Welcome to the rock paper scissors game.");
+
+while (playerScore != 3 && enemyScore != 3)
+{
+    Console.WriteLine("");
+    Console.WriteLine("Player Score: " + playerScore + ". Enemy Score: " + enemyScore);
+    Console.WriteLine("Please enter 'r' for rock, 'p' for paper or anything else for scissors.");
+    string playerChoice = Console.ReadLine();
+    
+
+
+    int enemyChoice = random.Next(0, 3);
+
+    if (enemyChoice == 0)
+    {
+        Console.WriteLine("Enemy chooses rock.");
+        switch (playerChoice)
+        {
+            case "r":
+                Console.WriteLine("Tie.");
+                break;
+            case "p":
+                Console.WriteLine("Player wins this round.");
+                playerScore++;
+                break;
+            default:
+                Console.WriteLine("Enemy wins this round");
+                enemyScore++;
+                break;
+        }
+    }
+    else if (enemyChoice == 1) {
+        Console.WriteLine("Enemy chooses paper.");
+        switch (playerChoice)
+        {
+            case "r":
+                Console.WriteLine("Enemy wins this round.");
+                enemyScore++;
+                break;
+            case "p":
+                Console.WriteLine("Tie.");
+                break;
+            default:
+                Console.WriteLine("Player wins this round");
+                playerScore++;
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Enemy chooses scissors.");
+        switch (playerChoice)
+        {
+            case "r":
+                Console.WriteLine("Player wins this round.");
+                playerScore++;
+                break;
+            case "p":
+                Console.WriteLine("Enemy wins this round.");
+                enemyScore++;
+                break;
+            default:
+                Console.WriteLine("Tie");
+                break;
+        }
+    }
+}
+
+if(playerScore == 3)
+{
+    Console.WriteLine("You win :D");
+}
+else
+{
+    Console.WriteLine("You loose :(");
+}
+
+
+
+Console.ReadKey();
