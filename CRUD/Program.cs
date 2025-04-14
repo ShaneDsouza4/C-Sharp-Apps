02019afda33f67f10dbb0dfba69cc5ab574eccf1
ShaneDsouza4 <shane.json96@gmail.com>
2025-04-13 23:50:04 -0400

    CRUD App

---
+++ CRUD/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Welcome to the CRUD App");
+List<string> TaskList = new List<string>();
+string option = "";
+
+while (option != "e")
+{
+    Console.WriteLine("What would you like to do ?");
+    Console.WriteLine("Press 1 to Add");
+    Console.WriteLine("Press 2 to Remove");
+    Console.WriteLine("Press 3 to View");
+    Console.WriteLine("Press e to Exit");
+
+
+    option = Console.ReadLine();
+
+    if(option == "1")
+    {
+        Console.WriteLine("Please enter the name of the task: ");
+        string task = Console.ReadLine();
+        TaskList.Add(task);
+    }
+    else if (option == "2")
+    {
+        for (int i = 0; i < TaskList.Count; i++)
+        {
+            Console.WriteLine(i + ": " + TaskList[i]);
+        }
+        Console.WriteLine("Please enter the task number to remove from the list: ");
+        int taskNumber = Convert.ToInt32(Console.ReadLine());
+        TaskList.RemoveAt(taskNumber);
+    }
+    else if (option == "3")
+    {
+        Console.WriteLine("Current Tasks in the list are;");
+        for (int i = 0; i < TaskList.Count; i++)
+        {
+            Console.WriteLine(i + ": " + TaskList[i]);
+        }
+    }
+    else if(option == "e")
+    {
+        Console.WriteLine("Exiting the program.");
+    }
+    else
+    {
+        Console.WriteLine("Invalid Option. Please try again.");
+    }
+}
+
+Console.WriteLine("Thank you for using the CRUD App to add Tasks.");
--- Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00003a0270619140be066e6003710704f75b1394")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f31f316cf6e0f34fa849fd684d5250830a49531")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
